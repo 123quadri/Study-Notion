d1d978ceac98eff3cb1d88866f21fb9ecf3033c3
123quadri <syed.moh09@gmail.com>
2023-07-20 16:27:33 +0530

    4th commit to Studynotion

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.use(express.json());
 
 app.use(
     cors({
-        origin:"http://localhost:3000",
+        origin:"https://studynotion-frontend-eight.vercel.app/",
         credentials:true,
     })
 );
